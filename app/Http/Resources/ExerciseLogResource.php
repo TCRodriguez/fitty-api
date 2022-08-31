c73b2383dc92d98e8c9e173c430e5db9f91e3eaa
Tonatiuh Rodriguez <tonatiuh@tcrodriguez.com>
2022-08-30 22:57:23 -0700

    enable exercise id and exercise name on log functionality

--- app/Http/Resources/ExerciseLogResource.php
+++ app/Http/Resources/ExerciseLogResource.php
@@ -18,6 +18,7 @@ class ExerciseLogResource extends JsonResource
             'created_at' => $this->resource->created_at,
             'id' => $this->resource->id,
             'workout_id' => $this->resource->workout_id,
+            'exercise_id' => $this->resource->exercise_id,
             'exercise_name' => optional($this->resource->exercise)->exercise_name,
             'sets' => $this->resource->sets,
             'reps' => $this->resource->reps,

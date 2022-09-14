1c8b4e3feeceb85dba82348ac82e5090faf791d1
Tonatiuh Rodriguez <tonatiuh@tcrodriguez.com>
2022-09-13 23:50:52 -0700

    implement update trainer controller action

--- app/Http/Controllers/TrainersController.php
+++ app/Http/Controllers/TrainersController.php
@@ -2,9 +2,46 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Trainer;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class TrainersController extends Controller
 {
-    //
+    public function store(Request $request)
+    {
+        // $hashedPassword = Hash::make($request->password);
+
+        // $trainer = Trainer::create([
+        //     'first_name' => $request->input('first_name'),
+        //     'last_name' => $request->input('last_name'),
+        //     'user_name' => $request->input('user_name'),
+        //     'email' => $request->input('email'),
+        //     'password' => Hash::make($request->password),
+        // ]);
+        return $request;
+    }
+
+    public function update(Request $request, Trainer $trainer)
+    {
+        // dd($request);
+        // $trainer = Trainer::findOrFail($trainer);
+
+        $trainer->update([
+            'first_name' => $request->input('first_name'),
+            'last_name' => $request->input('last_name'),
+            'user_name' => $request->input('user_name'),
+            'email' => $request->input('email'),
+            'password' => Hash::make($request->password), 
+        ]);
+
+        return $trainer;
+        // return $request;
+    }
+
+    public function destroy($id)
+    {
+
+    }
+    
 }

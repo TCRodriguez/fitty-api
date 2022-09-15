8eaa55b8b48acdfc582767c36fdd642d0aa82778
Tonatiuh Rodriguez <tonatiuh@tcrodriguez.com>
2022-09-15 00:23:11 -0700

    implement delete trainer route action

--- app/Http/Controllers/TrainersController.php
+++ app/Http/Controllers/TrainersController.php
@@ -10,15 +10,15 @@ class TrainersController extends Controller
 {
     public function store(Request $request)
     {
-        // $hashedPassword = Hash::make($request->password);
+        $hashedPassword = Hash::make($request->password);
 
-        // $trainer = Trainer::create([
-        //     'first_name' => $request->input('first_name'),
-        //     'last_name' => $request->input('last_name'),
-        //     'user_name' => $request->input('user_name'),
-        //     'email' => $request->input('email'),
-        //     'password' => Hash::make($request->password),
-        // ]);
+        $trainer = Trainer::create([
+            'first_name' => $request->input('first_name'),
+            'last_name' => $request->input('last_name'),
+            'user_name' => $request->input('user_name'),
+            'email' => $request->input('email'),
+            'password' => Hash::make($request->password),
+        ]);
         return $request;
     }
 
@@ -41,7 +41,11 @@ class TrainersController extends Controller
 
     public function destroy($id)
     {
+        $trainer = Trainer::findOrFail($id);
 
+        $trainer->delete();
+
+        return 'Trainer deleted.';
     }
     
 }
